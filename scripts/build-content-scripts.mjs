a3980581474326e389f263bd8bdb8bca22ec764b
sushidesu <hugemerge@gmail.com>
2022-07-15 21:16:16 +0900

    fix: use esbuild

--- scripts/build-content-scripts.mjs
+++ scripts/build-content-scripts.mjs
@@ -1,12 +1,10 @@
-import cpy from "cpy"
 import { execa } from "execa"
 import { globby } from "globby"
 
 const main = async () => {
-  await cpy("manifest.json", "dist")
   const contentScriptFiles = await globby("src/contents/*.ts")
   for (const filename of contentScriptFiles) {
-    await execa("yarn", ["swc", filename, "-d", "dist"])
+    await execa("yarn", ["esbuild", "--bundle", "--target=es2020", "--minify", "--outdir=dist/contents", filename])
   }
 }
 
