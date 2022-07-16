b3258ac3ba931445e50e574a386e2b91c6764d75
sushidesu <hugemerge@gmail.com>
2022-07-16 15:47:05 +0900

    fix

--- scripts/build-manifest.mjs
+++ scripts/build-manifest.mjs
@@ -3,7 +3,7 @@ import fs from "fs/promises"
 const main = async () => {
   const manifest = await fs.readFile("manifest.json", "utf8")
   const { $schema, ...rest } = JSON.parse(manifest)
-  fs.writeFile("dist/manifest.json", JSON.stringify(rest, undefined, 2), { flag: "w+" })
+  await fs.writeFile("dist/manifest.json", JSON.stringify(rest, undefined, 2), { flag: "w+" })
 }
 
 main()
