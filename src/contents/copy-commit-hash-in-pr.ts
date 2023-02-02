02c86dcf33275c9b0a91ee5423bcbdda0a761244
sushidesu <hugemerge@gmail.com>
2023-02-02 21:58:18 +0900

    refactor

--- src/contents/copy-commit-hash-in-pr.ts
+++ src/contents/copy-commit-hash-in-pr.ts
@@ -1,29 +1,39 @@
-import { Logger, createLogger } from "../utils/logger"
+import { Logger, createLogger } from "../utils/logger";
 
 const copyCommentHashInPR = (logger: Logger) => {
-  logger.messageOnLoaded()
-  const commits = document.querySelectorAll(".TimelineItem.TimelineItem--condensed")
+  logger.messageOnLoaded();
+  const commits = document.querySelectorAll(
+    ".TimelineItem.TimelineItem--condensed"
+  );
   for (const c of Array.from(commits.values())) {
-    const hashContainer = c.querySelector(".text-right.ml-1")
-    if (hashContainer === null) continue
+    const hashContainer = c.querySelector(".text-right.ml-1");
+    if (hashContainer === null) continue;
 
-    const hash = hashContainer.querySelector("code > a")?.textContent
-    const copyHashButton = document.createElement("button")
-    copyHashButton.textContent = "Copy"
-    copyHashButton.classList.add("btn", "btn-sm", "ml-2", "mb-1")
-    copyHashButton.onclick = () => {
-      if (hash) {
-        navigator.clipboard.writeText(hash)
-        copyHashButton.textContent = "Copied!"
-        logger.message(`copied commit hash ${hash}`)
-        setTimeout(() => {
-          copyHashButton.textContent = "Copy"
-        }, 2000)
-      }
-    }
-    c.appendChild(copyHashButton)
+    const hash = hashContainer.querySelector("code > a")?.textContent;
+    if (typeof hash !== "string") continue;
+
+    const copyHashButton = createCopyHashButton(hash);
+    c.appendChild(copyHashButton);
   }
-}
+};
 
-const logger = createLogger()
-copyCommentHashInPR(logger)
+const createCopyHashButton = (hash: string): HTMLButtonElement => {
+  const copyHashButton = document.createElement("button");
+  copyHashButton.id = `greedy-github-copy-hash-button-${hash}`;
+  copyHashButton.textContent = "Copy";
+  copyHashButton.classList.add("btn", "btn-sm", "ml-2", "mb-1");
+  copyHashButton.onclick = () => {
+    if (hash) {
+      navigator.clipboard.writeText(hash);
+      copyHashButton.textContent = "Copied!";
+      logger.message(`copied commit hash ${hash}`);
+      setTimeout(() => {
+        copyHashButton.textContent = "Copy";
+      }, 2000);
+    }
+  };
+  return copyHashButton;
+};
+
+const logger = createLogger();
+copyCommentHashInPR(logger);
